412274377b8aeb3cc36f30e6c650716d867bd78b
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-08-04 18:08:31 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"os"
+	"reflect"
+)
+
+func randomFloat(max, min float64) float64 {
+	return rand.Float64()*(max-min) + min
+}
+
+func main() {
+	data := make(map[string]interface{})
+
+	j, err := os.ReadFile("example.json")
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(j, &data)
+	for _, value := range data {
+		fmt.Println(value)
+		fmt.Println(reflect.TypeOf(value))
+		fmt.Println(reflect.TypeOf(value).Kind() == reflect.Slice)
+	}
+}
