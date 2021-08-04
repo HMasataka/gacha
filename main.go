9b5926f231e5a1ccaa116638593402b1358ccc95
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2021-08-04 19:05:09 +0900

    Feat run function

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"math/rand"
 	"os"
@@ -13,6 +14,27 @@ import (
 func randomFloat(max, min float64) float64 {
 	rand.Seed(time.Now().UnixNano())
 	return rand.Float64()*(max-min) + min
+}
+
+func toStrings(v []interface{}) []string {
+	s := make([]string, len(v))
+	for i, vv := range v {
+		s[i] = fmt.Sprint(vv)
+	}
+	return s
+}
+
+func toData(target interface{}) (Data, error) {
+	v, ok := target.(Data)
+	if ok {
+		return v, nil
+	}
+
+	vv, ok := target.(map[string]interface{})
+	if ok {
+		return Data(vv), nil
+	}
+	return nil, errors.New("cast error")
 }
 
 type Data map[string]interface{}
@@ -37,6 +59,41 @@ func (d Data) IsValidPercent() (bool, error) {
 	return percent == 100.0, nil
 }
 
+func do(data interface{}) (interface{}, error) {
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Map:
+		target, err := toData(data)
+		if err != nil {
+			return nil, err
+		}
+		isValidPercent, err := target.IsValidPercent()
+		if !isValidPercent || err != nil {
+			return nil, errors.New("invalid percentage")
+		}
+
+		var percent float64
+		r := randomFloat(100, 0)
+		for key, value := range target {
+			f, err := strconv.ParseFloat(key, 64)
+			if err != nil {
+				return nil, err
+			}
+			percent += f
+			if r < percent {
+				return do(value)
+			}
+		}
+	case reflect.Slice:
+		target := toStrings(data.([]interface{}))
+		r := rand.Intn(len(target))
+		return do(target[r])
+	case reflect.String:
+		return data, nil
+	}
+
+	return nil, errors.New("error")
+}
+
 func main() {
 	data := make(Data)
 
@@ -47,12 +104,10 @@ func main() {
 
 	json.Unmarshal(j, &data)
 
-	fmt.Println(data.Keys())
-	fmt.Println(data.IsValidPercent())
-
-	for _, value := range data {
-		fmt.Println(value)
-		fmt.Println(reflect.TypeOf(value))
-		fmt.Println(reflect.TypeOf(value).Kind() == reflect.Slice)
+	isValidPercent, err := data.IsValidPercent()
+	if !isValidPercent {
+		panic(errors.New("Invalid percent"))
 	}
+
+	fmt.Println(do(data))
 }
